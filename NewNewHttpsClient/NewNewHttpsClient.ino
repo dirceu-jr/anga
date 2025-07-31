99c0fba53e141b40204b88ee308d7272763e6f42
Dirceu Jr <dirceuu@gmail.com>
2025-07-30 22:05:09 -0300

    update sim7000g usage

---
+++ NewNewHttpsClient/NewNewHttpsClient.ino
@@ -0,0 +1,268 @@
+// TODO:
+// - Ping ThingSpeak Generic - DONE;
+// - Disable Wi-Fi - DONE;
+// - Disable Bluetooth - DONE;
+// - Check/disable GPS - DONE;
+// - ESP32 deep sleep - DONE;
+// - CO;
+// - VOC;
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM7000
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define SerialAT Serial1
+
+// Increase RX buffer to capture the entire response
+// Chips without internal buffering (A6/A7, ESP8266, M590)
+// need enough space in the buffer for the entire response
+// else data will be lost (and the http library will fail).
+#define TINY_GSM_RX_BUFFER 1024
+
+// See all AT commands, if wanted
+// #define DUMP_AT_COMMANDS
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+// #define LOGGING  // <- Logging is for the HTTP library
+
+// flag to force SSL client authentication, if needed
+// #define TINY_GSM_SSL_CLIENT_AUTHENTICATION
+
+// Your GPRS credentials, if any
+const char apn[] = "iot.datatem.com.br";
+const char gprsUser[] = "datatem";
+const char gprsPass[] = "datatem";
+
+// Server details
+const char server[] = "api.thingspeak.com";
+const int port = 80;
+
+const char* writeAPIKey = "N8URJD5TZVON1T54";
+
+#include "esp_wifi.h"
+#include "esp_bt.h"
+
+#include <TinyGsmClient.h>
+#include <ArduinoHttpClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm        modem(debugger);
+#else
+TinyGsm        modem(SerialAT);
+#endif
+
+TinyGsmClient client(modem);
+HttpClient    http(client, server, port);
+
+#define UART_BAUD   115200
+#define PIN_TX      27
+#define PIN_RX      26
+#define PWR_PIN     4
+#define LED_PIN     12
+
+void modemPowerOn() {
+  pinMode(PWR_PIN, OUTPUT);
+  digitalWrite(PWR_PIN, LOW);
+  delay(1000);    //Datasheet Ton mintues = 1S
+  digitalWrite(PWR_PIN, HIGH);
+}
+
+void modemPowerOff() {
+  pinMode(PWR_PIN, OUTPUT);
+  digitalWrite(PWR_PIN, LOW);
+  delay(1500);    //Datasheet Ton mintues = 1.2S
+  digitalWrite(PWR_PIN, HIGH);
+}
+
+void setup() {
+  // Set LED OFF
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  // Set console baud rate
+  SerialMon.begin(115200);
+  delay(1000);
+
+  // Disable Wi-Fi and Bluetooth
+  SerialMon.println(F("Disabling Wi-Fi and Bluetooth..."));
+  esp_wifi_stop();
+  esp_bt_controller_disable();
+
+  // modemPowerOff();
+  SerialMon.println(F("Power modem on..."));
+  modemPowerOn();
+
+  delay(1000);
+
+  // SerialMon.println("Wait...");
+
+  // Set GSM module baud rate
+  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+  delay(6000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Restating modem...");
+  // modem.restart();
+  modem.init();
+
+  // Disable GPS
+  modem.disableGPS();
+
+  // Set SIM7000G GPIO4 LOW ,turn off GPS power
+  // CMD:AT+SGPIO=0,4,1,0
+  // Only in version 20200415 is there a function to control GPS power
+  modem.sendAT("+SGPIO=0,4,1,0");
+  if (modem.waitResponse(10000L) != 1) {
+    SerialMon.println(" SGPIO=0,4,1,0 false ");
+  }
+
+  delay(200);
+
+  String name = modem.getModemName();
+  SerialMon.println("Modem Name: " + name);
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.println("Modem Info: " + modemInfo);
+
+  // Check SIM status
+  SerialMon.print("SIM Status: ");
+  SerialMon.println(modem.getSimStatus());
+
+  String res;
+  // 1 CAT-M
+  // 2 NB-IoT
+  // 3 CAT-M and NB-IoT
+  res = modem.setPreferredMode(2);
+  SerialMon.print("setPreferredMode: ");
+  SerialMon.println(res);
+
+  // 2 Automatic
+  // 13 GSM only
+  // 38 LTE only
+  // 51 GSM and LTE only
+  res = modem.setNetworkMode(2);
+  SerialMon.print("setNetworkMode: ");
+  SerialMon.println(res);
+}
+
+void loop() {
+  // modem.gprsConnect(apn, gprsUser, gprsPass);
+
+  SerialMon.print("Signal quality: ");
+  SerialMon.println(modem.getSignalQuality());
+
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+  // GPRS connection parameters are usually set after network registration
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS connected");
+  }
+
+  // Add debugging for local IP
+  IPAddress local = modem.localIP();
+  SerialMon.print("Local IP: ");
+  SerialMon.println(local);
+
+  // Now try HTTP request
+  http.setTimeout(30000);
+  // http.connectionKeepAlive();  // Currently, this is needed for HTTPS
+
+  // Create a sample value to send
+  int sensor1Value = random(100);
+  int sensor2Value = random(100);
+
+  SerialMon.print(F("Value 1 to send: "));
+  SerialMon.println(sensor1Value);
+
+  SerialMon.print(F("Value 2 to send: "));
+  SerialMon.println(sensor2Value);
+
+  // Construct the resource URL
+  String resource = String("/update?api_key=") + writeAPIKey + "&field1=" + String(sensor1Value) + "&field2=" + String(sensor2Value);
+
+  SerialMon.print(F("Performing HTTP POST request... "));
+  int err = http.post(resource);
+  if (err != 0) {
+    SerialMon.print(F("failed to connect, error: "));
+    SerialMon.println(err);
+    delay(10000);
+    return;
+  }
+
+  int status = http.responseStatusCode();
+  SerialMon.print(F("Response status code: "));
+  SerialMon.println(status);
+  if (!status) {
+    delay(10000);
+    return;
+  }
+
+  // SerialMon.println(F("Response Headers:"));
+  // while (http.headerAvailable()) {
+  //   String headerName  = http.readHeaderName();
+  //   String headerValue = http.readHeaderValue();
+  //   SerialMon.println("    " + headerName + " : " + headerValue);
+  // }
+
+  int length = http.contentLength();
+  if (length >= 0) {
+    SerialMon.print(F("Content length is: "));
+    SerialMon.println(length);
+  }
+  if (http.isResponseChunked()) {
+    SerialMon.println(F("The response is chunked"));
+  }
+
+  String body = http.responseBody();
+  SerialMon.println(F("Response:"));
+  SerialMon.println(body);
+
+  SerialMon.print(F("Body length is: "));
+  SerialMon.println(body.length());
+
+  // Shutdown
+
+  http.stop();
+  SerialMon.println(F("Server disconnected"));
+
+  modem.gprsDisconnect();
+  SerialMon.println(F("GPRS disconnected"));
+
+  // Power down the modem using AT command first
+  SerialMon.println(F("Powering down modem with AT command..."));
+  modem.sendAT("+CPOWD=1");
+  modem.waitResponse();
+
+  modemPowerOff();
+  SerialMon.println(F("Modem off"));
+
+  // sleep for 60 seconds
+  ESP.deepSleep(60e6);
+}
