f3c23be808411c202107568d9aa9e2b9bcf5e125
Dirceu Jr <dirceuu@gmail.com>
2025-09-07 21:47:45 -0300

    CO via https

--- HttpsClient_CO_NB_IOT/HttpsClient_CO_NB_IOT.ino
+++ HttpsClient_CO_NB_IOT/HttpsClient_CO_NB_IOT.ino
@@ -1,22 +1,10 @@
-// TODO:
-// - Ping ThingSpeak Generic - DONE;
-// - Disable Wi-Fi - DONE;
-// - Disable Bluetooth - DONE;
-// - Check/disable GPS - DONE;
-// - ESP32 deep sleep - DONE;
-// - CO - DONE;
-// - CO sensor is "on" when ESP32 is sleeping?
-// - VOC;
-// - Optimizations;
-
 // Select your modem:
-#define TINY_GSM_MODEM_SIM7000
+#define TINY_GSM_MODEM_SIM7000SSL
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
 
 // Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
 #define SerialAT Serial1
 
 // Increase RX buffer to capture the entire response
@@ -32,9 +20,6 @@
 #define TINY_GSM_DEBUG SerialMon
 // #define LOGGING  // <- Logging is for the HTTP library
 
-// flag to force SSL client authentication, if needed
-// #define TINY_GSM_SSL_CLIENT_AUTHENTICATION
-
 // Your GPRS credentials, if any
 const char apn[] = "iot.datatem.com.br";
 const char gprsUser[] = "datatem";
@@ -42,7 +27,7 @@ const char gprsPass[] = "datatem";
 
 // Server details
 const char server[] = "api.thingspeak.com";
-const int port = 80;
+const int port = 443;
 
 const char* writeAPIKey = "NEC73XT4UKMIPOZM";
 
@@ -65,7 +50,7 @@ TinyGsm        modem(debugger);
 TinyGsm        modem(SerialAT);
 #endif
 
-TinyGsmClient client(modem);
+TinyGsmClientSecure client(modem);
 HttpClient    http(client, server, port);
 
 #define UART_BAUD   115200
@@ -84,8 +69,9 @@ void modemPowerOn() {
 void modemPowerOff() {
   pinMode(PWR_PIN, OUTPUT);
   digitalWrite(PWR_PIN, LOW);
-  delay(1500); //Datasheet Ton mintues = 1.2S
+  delay(1200); //Datasheet Ton mintues = 1.2S
   digitalWrite(PWR_PIN, HIGH);
+  pinMode(PWR_PIN, INPUT);
 }
 
 void modemHardReset() {
@@ -97,6 +83,10 @@ void modemHardReset() {
 }
 
 void setup() {
+  // Disable Wi-Fi and Bluetooth
+  esp_wifi_stop();
+  esp_bt_controller_disable();
+
   // Set LED OFF
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
@@ -104,11 +94,6 @@ void setup() {
   // Set console baud rate
   SerialMon.begin(115200);
   delay(1000);
-
-  // Disable Wi-Fi and Bluetooth
-  SerialMon.println(F("Disabling Wi-Fi and Bluetooth..."));
-  esp_wifi_stop();
-  esp_bt_controller_disable();
 
   // gas sensor
   // Mode of obtaining data: the main controller needs to request the sensor for data
@@ -119,14 +104,7 @@ void setup() {
 
   // modem
   SerialMon.println(F("Power modem on..."));
-  // TODO:
-  // - if stall again, test with hard reset;
-  // modemHardReset();
-  modemPowerOn();
-
-  delay(1000);
-
-  // SerialMon.println("Wait...");
+  modemHardReset();
 
   // Set GSM module baud rate
   SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
@@ -134,9 +112,13 @@ void setup() {
 
   // Restart takes quite some time
   // To skip it, call init() instead of restart()
-  SerialMon.println("Restating modem...");
+  // SerialMon.println("Restarting modem...");
   // modem.restart();
+  SerialMon.println("Initializing modem...");
   modem.init();
+
+  // update modem clock
+  modem.sendAT("+CCLK=\"25/09/07,01:29:00\"");
 
   // Disable GPS
   modem.disableGPS();
@@ -179,8 +161,6 @@ void setup() {
 }
 
 void loop() {
-  // modem.gprsConnect(apn, gprsUser, gprsPass);
-
   SerialMon.print("Signal quality: ");
   SerialMon.println(modem.getSignalQuality());
 
@@ -197,33 +177,32 @@ void loop() {
   }
 
   // GPRS connection parameters are usually set after network registration
-  SerialMon.print(F("Connecting to "));
-  SerialMon.print(apn);
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    SerialMon.println(" fail");
-    delay(10000);
-    return;
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS already connected.");
+  } else {
+    SerialMon.print(F("Connecting to "));
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+      SerialMon.println(" fail");
+      delay(10000);
+      return;
+    }
+    SerialMon.println(" success");
   }
-  SerialMon.println(" success");
 
   if (modem.isGprsConnected()) {
-    SerialMon.println("GPRS connected");
+    SerialMon.println("GPRS connected.");
   }
-
-  // Add debugging for local IP
-  IPAddress local = modem.localIP();
-  SerialMon.print("Local IP: ");
-  SerialMon.println(local);
 
   // Now try HTTP request
   http.setTimeout(30000);
-  // http.connectionKeepAlive(); // this may be needed for HTTPS
+  http.connectionKeepAlive(); // this may be needed for HTTPS
 
   // Construct the resource URL
   String resource = String("/update?api_key=") + writeAPIKey + "&field1=" + String(gas.readGasConcentrationPPM());
 
-  SerialMon.print(F("Performing HTTP POST request... "));
-  int err = http.post(resource);
+  SerialMon.print(F("Performing HTTPS GET request... "));
+  int err = http.get(resource);
   if (err != 0) {
     SerialMon.print(F("failed to connect, error: "));
     SerialMon.println(err);
@@ -239,28 +218,9 @@ void loop() {
     return;
   }
 
-  // SerialMon.println(F("Response Headers:"));
-  // while (http.headerAvailable()) {
-  //   String headerName  = http.readHeaderName();
-  //   String headerValue = http.readHeaderValue();
-  //   SerialMon.println("    " + headerName + " : " + headerValue);
-  // }
-
-  int length = http.contentLength();
-  if (length >= 0) {
-    SerialMon.print(F("Content length is: "));
-    SerialMon.println(length);
-  }
-  if (http.isResponseChunked()) {
-    SerialMon.println(F("The response is chunked"));
-  }
-
   String body = http.responseBody();
   SerialMon.println(F("Response:"));
   SerialMon.println(body);
-
-  SerialMon.print(F("Body length is: "));
-  SerialMon.println(body.length());
 
   // Shutdown
 
@@ -280,13 +240,6 @@ void loop() {
   modemPowerOff();
   SerialMon.println(F("Modem off"));
 
-  delay(1000);
-
-  // sleep for 60 seconds
-  ESP.deepSleep(60e6);
-
-  // TODO:
-  // - bug, it stoped to work but still with batery power;
-  // - started to work again after batery
-  // - may turn modem off/on on setup()?;
+  // sleep for 5 minutes
+  ESP.deepSleep(300e6);
 }
